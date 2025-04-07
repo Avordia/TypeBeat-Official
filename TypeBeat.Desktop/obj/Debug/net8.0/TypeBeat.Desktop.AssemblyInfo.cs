3879e56a2ca67dd2967853b52a695f1f93388b5d
Avordia <aesopianproductions@gmail.com>
2025-04-07 20:31:41 +0800

    Osu! Framework Installation

--- TypeBeat.Desktop/obj/Debug/net8.0/TypeBeat.Desktop.AssemblyInfo.cs
+++ TypeBeat.Desktop/obj/Debug/net8.0/TypeBeat.Desktop.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TypeBeat")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("0.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("0.0.0+3587de6790618482973d88c2b537ad04e701b4e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("TypeBeat")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TypeBeat")]
 [assembly: System.Reflection.AssemblyVersionAttribute("0.0.0.0")]
